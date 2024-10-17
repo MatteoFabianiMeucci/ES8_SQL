32847c699b3d7123ba98ec0a54af8d59ed634897
MatteoFabianiMeucci <matteofabiani06@gmail.com>
2024-10-17 10:20:04 +0200

    Collegamento fra tabelle con il JOIN

--- ES8.sql
+++ ES8.sql
@@ -1,45 +1,47 @@
 -- 6 Trovare il totale del bilancio dei progetti in cui ci sono impiegati che lavorano a Milano
 SELECT SUM(Progetto.bilancio) AS 'Totale bilancio di progetti di Milano'
-FROM Impiegato, Dipartimento, Progetto, Partecipazione
-WHERE Partecipazione.impiegato = Impiegato.matricola
-	AND Partecipazione.sigla_progetto = Progetto.sigla
-    AND Impiegato.codice_dipartimento = Dipartimento.codice
-    AND Dipartimento.sede = 'Milano';
-    
+FROM impiegato
+JOIN dipartimento ON (impiegato.codice_dipartimento = dipartimento.codice)
+JOIN partecipazione ON (impiegato.matricola = partecipazione.impiegato)
+JOIN progetto ON (progetto.sigla = partecipazione.sigla_progetto)
+WHERE dipartimento.sede = 'Milano';
+
 -- 7 Trovare i nomi dei progetti con bilancio maggiore di 10000 e i cognomi degli impiegati che lavorano su di essi.
 SELECT Progetto.Nome, Impiegato.cognome
-FROM Partecipazione, Impiegato, Progetto
-WHERE Partecipazione.impiegato = Impiegato.matricola
-	AND Partecipazione.sigla_progetto = Progetto.sigla
-    AND Progetto.bilancio > 10000;
+FROM partecipazione
+JOIN impiegato ON (partecipazione.impiegato = impiegato.matricola)
+JOIN progetto ON (progetto.sigla = partecipazione.sigla_progetto)
+WHERE Progetto.bilancio > 10000;
 
 -- 8 Trovare cognome degli impiegati che guadagnano più del loro direttore di dipartimento
 SELECT i1.cognome
-FROM Impiegato AS i1, Impiegato AS i2, Dipartimento
-WHERE i1.codice_dipartimento = Dipartimento.codice
-	AND i2.codice_dipartimento = Dipartimento.codice
-    AND i2.matricola = Dipartimento.matricola_direttore
+FROM impiegato AS i1
+JOIN dipartimento ON (i1.codice_dipartimento = dipartimento.codice)
+JOIN impiegato AS i2 ON (i2.codice_dipartimento = dipartimento.codice)
+WHERE i2.matricola = Dipartimento.matricola_direttore
     AND i1.matricola <> i2.matricola
     AND i1.stipendio > i2.stipendio;
     
 -- 9 Trovare nomi dei dipartimenti in cui lavorano impiegati che guadagnano più di 60000.
 SELECT Dipartimento.Nome
-FROM Impiegato, Dipartimento
-WHERE Impiegato.codice_dipartimento = Dipartimento.codice
-AND Impiegato.stipendio > 60000;
+FROM impiegato
+JOIN dipartimento ON (impiegato.codice_dipartimento = dipartimento.codice)
+AND impiegato.stipendio > 60000;
 
 -- 10 Matricola degli impiegati che hanno partecipato almeno ad un progetto in cui il responsabile è un impiegato del loro stesso dipartimento
 SELECT impiegato.matricola
-FROM Impiegato, Progetto, Partecipazione, Impiegato AS Responsabile
-WHERE Partecipazione.impiegato = Impiegato.matricola
-	AND Partecipazione.sigla_progetto = Progetto.sigla
-    AND Responsabile.matricola = Progetto.matricola_responsabile
-    AND Impiegato.matricola <> Responsabile.matricola
-    AND impiegato.codice_dipartimento = Responsabile.codice_dipartimento;
-
+FROM impiegato
+JOIN partecipazione ON (impiegato.matricola = partecipazione.impiegato)
+JOIN progetto ON (progetto.sigla = partecipazione.sigla_progetto)
+JOIN impiegato AS responsabile
+WHERE responsabile.matricola = progetto.matricola_responsabile
+    AND impiegato.matricola <> responsabile.matricola
+    AND impiegato.codice_dipartimento = responsabile.codice_dipartimento;
+    
 -- 11 La quantità dei progetti con responsabili che lavorano in una città a scelta (es. Milano)
 SELECT COUNT(Progetto.sigla) AS 'quantità dei progetti con responsabili che lavorano a Napoli'
-FROM Progetto, Impiegato, Dipartimento
-WHERE Progetto.matricola_responsabile = Impiegato.matricola
-	AND Impiegato.codice_dipartimento = Dipartimento.codice
+FROM impiegato
+JOIN dipartimento ON (impiegato.codice_dipartimento = dipartimento.codice)
+JOIN progetto
+WHERE progetto.matricola_responsabile = impiegato.matricola
     AND dipartimento.sede = 'Napoli';
